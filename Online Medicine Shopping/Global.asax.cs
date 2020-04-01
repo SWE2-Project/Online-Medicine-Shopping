4e523ecba1bbd6b57e58c7a929a3ab17333501e6
mohamedhussein8 <58297429+mohamedhussein8@users.noreply.github.com>
2020-04-01 18:31:44 +0200

    Update Global.asax.cs

--- Online Medicine Shopping/Global.asax.cs
+++ Online Medicine Shopping/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Online_Medicine_Shopping.DBContext;
 
 namespace Online_Medicine_Shopping
 {
@@ -16,6 +17,7 @@ namespace Online_Medicine_Shopping
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer<Online_Medicine_Shopping.DBContext.TemporaryDBContext>(null);
         }
     }
 }
