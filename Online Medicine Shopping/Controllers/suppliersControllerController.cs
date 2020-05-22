5b09e10aedcaa00fef50d9d6f51a8355701e6b78
“ahmedrashad12” <boda.010003@gmail.com>
2020-05-22 23:53:46 +0200

    Delete Supplier, Edit Supplier and  List of Supplier.

--- Online Medicine Shopping/Controllers/suppliersControllerController.cs
+++ Online Medicine Shopping/Controllers/suppliersControllerController.cs
@@ -21,6 +21,20 @@ namespace Online_Medicine_Shopping.Controllers
             return View(db.suppliers.ToList());
         }
 
+        // GET: suppliers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            supplier supplier = db.suppliers.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
 
         // GET: suppliers/Create
         public ActionResult Create()
@@ -45,12 +59,70 @@ namespace Online_Medicine_Shopping.Controllers
             return View(supplier);
         }
 
-       
+        // GET: suppliers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            supplier supplier = db.suppliers.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
 
+        // POST: suppliers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,location,email")] supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(supplier).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(supplier);
+        }
 
+        // GET: suppliers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            supplier supplier = db.suppliers.Find(id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(supplier);
+        }
 
-       
+        // POST: suppliers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            supplier supplier = db.suppliers.Find(id);
+            db.suppliers.Remove(supplier);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
